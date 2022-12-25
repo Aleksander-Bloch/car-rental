a082ca052001402b300822e8f27fbe5d8599f131
xvisox <hubertmirek@gmail.com>
2022-12-25 20:22:06 +0100

    Changes by alek

--- src/main/java/mimuw/CarLot.java
+++ src/main/java/mimuw/CarLot.java
@@ -34,14 +34,7 @@ public class CarLot {
             while (resultSet.next()) {
                 var car = new RentedCar();
                 // Car data.
-                car.setCarId(resultSet.getInt("car_id"));
-                car.setBrand(resultSet.getString("brand"));
-                car.setModel(resultSet.getString("model"));
-                car.setHorsepower(resultSet.getInt("horsepower"));
-                car.setYear(resultSet.getInt("year"));
-                car.setMileage(resultSet.getInt("mileage"));
-                car.setGearbox(resultSet.getString("gearbox"));
-                car.setCategory(resultSet.getString("category"));
+                downloadCar(resultSet, car);
 
                 // Rental data.
                 car.setStartDate(getDate(resultSet.getString("start_date")));
@@ -54,5 +47,42 @@ public class CarLot {
             System.out.println("Failed to create statement.");
             throw new RuntimeException(ex);
         }
+    }
+
+    public void getAvailableCars(FilterData filter) {
+        // Clear car lot on reload.
+        cars.clear();
+        System.out.println("Querying for all available cars...");
+        var connection = DatabaseManager.getInstance().connection();
+        try {
+            // Retrieving user's cars from the database.
+            String sql = "SELECT * FROM car WHERE car_id NOT IN (SELECT car_id FROM rental)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            // Execute the query and get the result set.
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                var car = new Car();
+                // Car data.
+                downloadCar(resultSet, car);
+                cars.add(car);
+            }
+            System.out.println("Query executed successfully. Found " + cars.size() + " cars.");
+        } catch (SQLException ex) {
+            System.out.println("Failed to create statement.");
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private void downloadCar(ResultSet resultSet, Car car) throws SQLException {
+        car.setCarId(resultSet.getInt("car_id"));
+        car.setBrand(resultSet.getString("brand"));
+        car.setModel(resultSet.getString("model"));
+        car.setHorsepower(resultSet.getInt("horsepower"));
+        car.setYear(resultSet.getInt("year"));
+        car.setMileage(resultSet.getInt("mileage"));
+        car.setGearbox(resultSet.getString("gearbox"));
+        car.setCategory(resultSet.getString("category"));
     }
 }
