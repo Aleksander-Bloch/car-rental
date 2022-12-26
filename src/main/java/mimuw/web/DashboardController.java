521c4cadf50faa6db6b8075e95a17a4080b7e624
Aleksander Bloch <alekbloch21@gmail.com>
2022-12-26 21:57:42 +0100

    Add logout functionality

--- src/main/java/mimuw/web/DashboardController.java
+++ src/main/java/mimuw/web/DashboardController.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import mimuw.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -22,4 +24,10 @@ public class DashboardController {
         return "dashboard";
     }
 
+    @PostMapping("/logout")
+    public String logout(SessionStatus sessionStatus) {
+        System.out.println("Logging out...");
+        sessionStatus.setComplete();
+        return "redirect:/login";
+    }
 }
